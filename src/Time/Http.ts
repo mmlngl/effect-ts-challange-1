427f0a47d1d922e75c9b252107a6b8e01b0b0bb5
Michele Memoli <michele.memoli@gmail.com>
2026-02-04 12:46:44 +0700

    feat(app): Added real impl

--- src/Time/Http.ts
+++ src/Time/Http.ts
@@ -1,11 +1,13 @@
 import * as P from "@effect/platform";
 import * as Api from "../Api";
+import * as Effect from 'effect/Effect'
+import * as DateTime from 'effect/DateTime'
 
 export const TimeGroup = P.HttpApiBuilder.group(
-	Api.ServerApi,
-	"time",
-	(handlers) =>
-		handlers.handle("getTime", () => {
-			return P.HttpServerResponse.text("yo");
-		}),
-);
+  Api.ServerApi,
+  "time",
+  (handlers) =>
+    handlers.handle("getTime", () => Effect.gen(function* () {
+      const now = yield* DateTime.now.pipe(Effect.andThen(d => DateTime.formatIso(d)));
+      return P.HttpServerResponse.text(now)
+    })))
